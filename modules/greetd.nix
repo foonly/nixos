a975f0f70bd50cfbfd16dfc1e0352a538a2b37be
Niklas Schönberg <niklas@uplink.fi>
2024-03-11 23:50:21 +0200

    66 current  2024-03-11 23:50:15  24.05pre590113.1536926ef562  6.6.18                          *

--- modules/greetd.nix
+++ modules/greetd.nix
@@ -9,16 +9,20 @@
   programs.regreet.cageArgs = ["-s" "-m" "last"];
   programs.regreet.settings = {
     background = {
-      path = "/home/niklas/.local/share/wallpapers/end_4.png";
+      path = "/home/niklas/.local/share/wallpapers/garden-lantern.jpg";
       fit = "Cover";
     };
 
     GTK = {
       application_prefer_dark_theme = true;
       cursor_theme_name = "Adwaita";
-      font_name = "Comic Code 16";
+      font_name = "Comic Neue 16";
       icon_theme_name = "Adwaita";
       theme_name = "Adwaita";
     };
   };
+
+  environment.systemPackages = with pkgs; [
+    comic-neue
+  ];
 }
