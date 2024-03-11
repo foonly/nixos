e1546958133bb9b64791d60f1865479ea16d5d46
Niklas Schönberg <niklas@uplink.fi>
2024-03-11 23:41:54 +0200

    64 current  2024-03-11 23:41:49  24.05pre590113.1536926ef562  6.6.18                          *

--- modules/greetd.nix
+++ modules/greetd.nix
@@ -7,4 +7,18 @@
   services.greetd.enable = true;
   programs.regreet.enable = true;
   programs.regreet.cageArgs = ["-s" "-m" "last"];
+  programs.regreet.settings = {
+    background = {
+      path = "/home/niklas/.local/share/wallpapers/end_4.png";
+      fit = "Cover";
+    };
+
+    GTK = {
+      application_prefer_dark_theme = true;
+      cursor_theme_name = "Adwaita";
+      font_name = "Cantarell 16";
+      icon_theme_name = "Adwaita";
+      theme_name = "Adwaita";
+    };
+  };
 }
