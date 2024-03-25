ad826fbc7c4dc19e8e2e422b45163094d9931b30
Niklas Schönberg <niklas@uplink.fi>
2024-03-25 23:02:12 +0200

    32 current  2024-03-25 23:02:08  24.05pre596954.d691274a972b  6.6.21                          *

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -7,7 +7,7 @@
     enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    gamescopeSession.enable = false;
+    gamescopeSession.enable = true;
   };
 
   environment.systemPackages = with pkgs; [
