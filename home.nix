be6026db793fc449b0e3b7fafb74b651e2fcd77f
Niklas Schönberg <niklas@uplink.fi>
2024-03-25 21:07:03 +0200

    101 current  2024-03-25 21:06:58  24.05pre600876.20f77aa09916  6.6.22                          *

--- home.nix
+++ home.nix
@@ -28,18 +28,9 @@
 
     programs.starship = {
       enable = true;
-      # Configuration written to ~/.config/starship.toml
-      settings = {
-        # add_newline = false;
-
-        # character = {
-        #   success_symbol = "[➜](bold green)";
-        #   error_symbol = "[➜](bold red)";
-        # };
-
-        # package.disabled = true;
-      };
     };
+
+    services.ssh-agent.enable = true;
 
     home.pointerCursor = {
       gtk.enable = true;
