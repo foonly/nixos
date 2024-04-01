0fc28c8e462572f4aea0b05584d67d7e502ddaf3
Niklas Schönberg <niklas@uplink.fi>
2024-04-01 23:06:13 +0300

    117 current  2024-04-01 23:06:09  24.05pre603596.2726f127c15a  6.6.22                          *

--- modules/greetd.nix
+++ modules/greetd.nix
@@ -3,25 +3,10 @@
   pkgs,
   ...
 }: {
-  #services.cage.enable = true;
   services.greetd.enable = true;
-  programs.regreet.enable = true;
-  programs.regreet.cageArgs = ["-s" "-m" "last"];
-  programs.regreet.settings = {
-    background = {
-      path = "/home/niklas/.local/share/wallpapers/garden-lantern.jpg";
-      fit = "Cover";
-    };
-
-    GTK = {
-      application_prefer_dark_theme = true;
-      cursor_theme_name = "Adwaita";
-      font_name = "Comic Neue 16";
-      icon_theme_name = "Adwaita";
-      theme_name = "Adwaita";
+  services.greetd.settings = {
+    default_session = {
+      command = "${pkgs.greetd.greetd}/bin/agreety --cmd Hyprland";
     };
   };
-
-  environment.systemPackages = with pkgs; [
-  ];
 }
